d99edaf837461dce2d4e611636923c5104f16e03
rahu7v3rma <rv.apple.1994@icloud.com>
2025-01-19 15:39:01 +0530

    remove start shell file

--- start.sh
+++
@@ -1,7 +0,0 @@
-docker stop kacifyfrontend || true
-docker rm kacifyfrontend || true
-docker stop kacifyapi || true
-docker rm kacifyapi || true
-docker build -t kacify-frontend ./frontend
-docker build -t kacify-api ./api
-docker compose up -d
